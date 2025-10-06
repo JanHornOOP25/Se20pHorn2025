dc09808a68c73a3e860e4024e9b02edc9074b7b6
theoandrup <theo.andrup@t-online.de>
2025-10-06 13:51:51 +0200

    fuck

--- Uebung_1_Git_Ent-wicklerJan/src/gui/Main.java
+++ Uebung_1_Git_Ent-wicklerJan/src/gui/Main.java
@@ -1,5 +1,15 @@
 package gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		new Anwendungssystem(primaryStage);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
