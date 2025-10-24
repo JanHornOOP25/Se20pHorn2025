a247912513101cf2decca4945d5e3fa78eca49c5
speed <speed@Darklightspc>
2025-10-24 11:04:54 +0200

    mesage

--- Basisanwendung/src/gui/Main.java
+++ Basisanwendung/src/gui/Main.java
@@ -1,10 +1,15 @@
 package gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		System.out.print(true);
-
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		new Anwendungssystem(primaryStage);
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
