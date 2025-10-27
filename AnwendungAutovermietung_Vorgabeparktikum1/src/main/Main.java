c7f360e22b6881d92f46d33d1eb48a80328baed6
Student <otelsorin@gmail.com>
2025-10-27 15:18:13 +0100

    yc

--- AnwendungAutovermietung_Vorgabeparktikum1/src/main/Main.java
+++ AnwendungAutovermietung_Vorgabeparktikum1/src/main/Main.java
@@ -9,6 +9,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		new AutovermietungAnwendungssystem(primaryStage);
+		sad
 	}	
 	
 	public static void main(String[] args){
